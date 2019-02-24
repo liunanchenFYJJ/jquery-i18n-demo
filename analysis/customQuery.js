3f79a53331e79b9b7f10aee955359ad45a10c586
LiunanChen <liunan_chen@163.com>
2019-02-24 23:03:03 +0800

    update cQuery

--- analysis/customQuery.js
+++ analysis/customQuery.js
@@ -2,24 +2,30 @@
 // 引入window: 将window变成局部变量，使用时不必将作用域链回退到顶级作用域
 //     undefined: 确保undefined为undefined?
 (function(window, undefined) {
-    // 定义cQuery
-    let cQuery = function() {
-        console.log('cQuery');
+    // 定义cQuery对象
+    /**
+     * 
+     * @param {String} selector 
+     * @returns {Object} cQuery
+     */
+    let cQuery = function( selector ) {
+        return new cQuery.fn.init( selector );
     };
     // 版本号
     let version = '0.0.1';
-    // 为什么要这么写?
+    // cQuery对象原型 
+    // 为什么要这么写:
+    // 所有挂载到cQuery.fn的方法，相当于挂载到cQuery.prototype,即cQuery函数上
     cQuery.fn = cQuery.prototype = {
         cquery: version,
         constructor: cQuery,
         length: 0,
-        add: function() {
-
-        },
-        plus: function() {
+        init: function( selector ) {
 
         }
     };
+    // 
+    cQuery.fn.init.prototype = cQuery.fn;
     // 暴露cQuery
     window.$ = window.cQuery = cQuery; 
 })(window)
