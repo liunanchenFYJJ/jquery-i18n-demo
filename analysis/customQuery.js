69dce49110a291e74b61def72d454f8745f03871
LiunanChen <liunan_chen@163.com>
2019-02-25 21:35:52 +0800

    cQuery architecture

--- analysis/customQuery.js
+++ analysis/customQuery.js
@@ -4,27 +4,31 @@
 (function(window, undefined) {
     // 定义cQuery对象
     /**
-     * 
+     * 构造函数cQuery
+     * 原型模式
      * @param {String} selector 
-     * @returns {Object} cQuery
+     * @returns {cQuery} cQuery实例
      */
     let cQuery = function( selector ) {
+        // return new cQuery.prototype.init( selector );
         return new cQuery.fn.init( selector );
     };
     // 版本号
     let version = '0.0.1';
     // cQuery对象原型 
-    // 为什么要这么写:
-    // 所有挂载到cQuery.fn的方法，相当于挂载到cQuery.prototype,即cQuery函数上
+    // 为什么要这么写: cQuery.fn的作用,扩展?
+    // 所有挂载到cQuery.fn的方法,都相当于挂载到cQuery原型
     cQuery.fn = cQuery.prototype = {
-        cquery: version,
+        // construcotor默认指向Object,改变其指向:原构造函数
         constructor: cQuery,
+        cquery: version,
         length: 0,
         init: function( selector ) {
-
+            console.log('selector');
         }
     };
-    // 
+    // 对应第13行?
+    // cQuery.fn.init的原型 ==指向==> cQuery.fn 即$实例继承cQuery.fn 所有方法
     cQuery.fn.init.prototype = cQuery.fn;
     // 暴露cQuery
     window.$ = window.cQuery = cQuery; 
--- analysis/test.js
+++ analysis/test.js
@@ -1,2 +1,23 @@
-let c = new cQuery();
-console.log(c);
+console.log($());
+
+// function person(name) {
+//     this.name = name;
+//     this.sayName = function() {
+//         console.log(this.name);
+//     }
+// }
+
+// function person(name) {
+//     this.name = name;
+// }
+// person.prototype.sayName = function() {
+//     console.log('factory ' + this.name);
+// }
+
+// let p = new person('kk');
+// console.log(p);
+// p.sayName();
+// console.log(p instanceof person);
+// // let p1 = person('jj');
+// // console.log(p1);
+// // window.sayName();
